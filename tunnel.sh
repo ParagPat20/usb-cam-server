4c16cc9dcbe5ded173adbb646e606f2db50d6843
Paragpat20 <parag.pat20@gmail.com>
2025-06-14 12:12:02 +0530

    tunnel

--- tunnel.sh
+++ tunnel.sh
@@ -10,5 +10,5 @@ REMOTE_PORT=8080
 # Ensure the key file has correct permissions
 chmod 400 $EC2_KEY_PATH
 
-# Create the SSH tunnel
-ssh -N -R $REMOTE_PORT:localhost:$LOCAL_PORT -i $EC2_KEY_PATH $EC2_USER@$EC2_HOST 
+# Create the SSH tunnel with -g flag to allow remote hosts to connect
+ssh -N -g -R 0.0.0.0:$REMOTE_PORT:localhost:$LOCAL_PORT -i $EC2_KEY_PATH $EC2_USER@$EC2_HOST 
