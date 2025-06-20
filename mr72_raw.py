2c4f13054405b0214c11b0ab40755b0b689321a3
Paragpat20 <parag.pat20@gmail.com>
2025-06-20 14:20:18 +0530

    esf

--- mr72_raw.py
+++ mr72_raw.py
@@ -18,14 +18,23 @@ def crc8_generic(buf, polynomial=0x07, initial_value=0x00):
 
 buffer = b''
 
+polys = [0x07, 0x31, 0x1D, 0x9B]
+inits = [0x00, 0xFF]
+
 while True:
     buffer += ser.read(64)
     while len(buffer) >= FRAME_LEN:
         if buffer[0:2] == HEADER:
             frame = buffer[:FRAME_LEN]
-            # CRC8 check: first 18 bytes, compare to 19th
-            if crc8_generic(frame[:18], polynomial=0x07, initial_value=0x00) == frame[18]:
-                print(frame)
+            data = frame[:18]
+            recv_crc = frame[18]
+            print(f"Frame: {frame.hex()}")
+            for poly in polys:
+                for init in inits:
+                    calc_crc = crc8_generic(data, polynomial=poly, initial_value=init)
+                    match = calc_crc == recv_crc
+                    print(f"  Poly: {hex(poly)}, Init: {hex(init)}, Calc CRC: {hex(calc_crc)}, Recv CRC: {hex(recv_crc)}, Match: {match}")
+            print("-"*60)
             buffer = buffer[FRAME_LEN:]
         else:
             buffer = buffer[1:] 
